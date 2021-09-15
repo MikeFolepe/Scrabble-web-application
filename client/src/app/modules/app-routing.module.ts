f699da712fc37ef9f0d1be951ebe1ba295d733bd
fienfiej <71525018+fienfiej@users.noreply.github.com>
2021-09-15 16:59:54 -0400

    Ajout page 404

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from '@app/components/page-not-found/page-not-found.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
-    { path: '**', redirectTo: '/home' },
+    { path: 'page-not-found', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/page-not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
