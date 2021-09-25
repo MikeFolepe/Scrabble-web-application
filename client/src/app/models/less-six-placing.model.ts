67d613aadca158b2beed69307f636d62e807e466
Majid Djido <abdoulmajid.djido@yahoo.com>
2021-09-25 04:41:30 -0400

    oublie d'un import

--- client/src/app/models/less-six-placing.model.ts
+++ client/src/app/models/less-six-placing.model.ts
@@ -1,4 +1,5 @@
 import { PlaceLetters } from './place-letter-strategy.model';
+import { PlayerIA } from './player-ia.model';
 export class LessSix extends PlaceLetters {
     execute(player: PlayerIA): void {
         // do nothing
