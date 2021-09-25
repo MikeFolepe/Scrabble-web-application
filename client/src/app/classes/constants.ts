f94e25d9d6354ecb48f0d16ca68b2ddd7acceda9
fienfiej <71525018+fienfiej@users.noreply.github.com>
2021-09-25 00:12:37 -0400

    Commentaires demo appliqués
    changer langue settings et font-size
    fix css boite de communication

--- client/src/app/classes/constants.ts
+++ client/src/app/classes/constants.ts
@@ -7,7 +7,7 @@ export const BOARD_SIZE = 16;
 export const EASEL_SIZE = 7;
 export const PLAYERS_NUMBER = 2;
 export const FONT_SIZE_MAX = 20;
-export const FONT_SIZE_MIN = 5;
+export const FONT_SIZE_MIN = 10;
 export const DEFAULT_FONT_SIZE = 13;
 export const SIZE_VARIATION = 1;
 export enum MouseButton {
