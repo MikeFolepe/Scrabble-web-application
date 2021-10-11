a69cd5ad65a54f76f60611558d31be5a88b28475
paako781 <paul-marie.akoffodji@polymtl.ca>
2021-10-11 17:22:30 -0400

    Test debug ok avec Lint

--- client/src/app/models/player-ia.model.spec.ts
+++ client/src/app/models/player-ia.model.spec.ts
@@ -8,81 +8,87 @@ import { PlayerIA } from './player-ia.model';
 // import { SkipTurn } from './skip-turn-strategy.model';
 // import { SwapLetter } from './swap-letter-strategy.model';
 
-// describe('PlayerIA', () => {
-//     const id = 0;
-//     const name = 'Player 1';
-//     const letterTable = [
-//         { value: 'A', quantity: 0, points: 0 },
-//         { value: 'B', quantity: 0, points: 0 },
-//         { value: 'C', quantity: 0, points: 0 },
-//         { value: 'D', quantity: 0, points: 0 },
-//         { value: 'E', quantity: 0, points: 0 },
-//         { value: 'F', quantity: 0, points: 0 },
-//         { value: 'G', quantity: 0, points: 0 },
-//     ];
+describe('PlayerIA', () => {
+    const id = 0;
+    const name = 'Player 1';
+    const letterTable = [
+        { value: 'A', quantity: 0, points: 0 },
+        { value: 'B', quantity: 0, points: 0 },
+        { value: 'C', quantity: 0, points: 0 },
+        { value: 'D', quantity: 0, points: 0 },
+        { value: 'E', quantity: 0, points: 0 },
+        { value: 'F', quantity: 0, points: 0 },
+        { value: 'G', quantity: 0, points: 0 },
+    ];
 
-//     let playerIA: PlayerIA;
+    let playerIA: PlayerIA;
 
-//     beforeEach(() => {
-//         playerIA = new PlayerIA(id, name, letterTable);
-//     });
+    beforeEach(() => {
+        playerIA = new PlayerIA(id, name, letterTable);
+    });
 
-    // it('should create an instance', () => {
-    //     expect(playerIA).toBeTruthy();
-    // });
+    it('should create an instance', () => {
+        expect(playerIA).toBeTruthy();
+    });
 
-    // it('should set a strategy', () => {
-    //     spyOn<any>(playerIA, 'generateRandomNumber').and.returnValues(0, 3, 5, 22);
+    // it('should set the respective strategy based on random numbers', () => {
     //     spyOn<any>(playerIA, 'pointingRange').and.returnValue({ min: 0, max: 6 });
 
+    //     spyOn<any>(playerIA, 'generateRandomNumber').and.returnValue(0);
     //     playerIA['setStrategy']();
     //     expect(playerIA.strategy).toBeInstanceOf(PlaceLetters);
+
+    //     playerIA['generateRandomNumber'] = jasmine.createSpy().and.returnValue(3);
     //     playerIA['setStrategy']();
     //     expect(playerIA.strategy).toBeInstanceOf(SkipTurn);
+
+    //     playerIA['generateRandomNumber'] = jasmine.createSpy().and.returnValue(5);
     //     playerIA['setStrategy']();
     //     expect(playerIA.strategy).toBeInstanceOf(SwapLetter);
+
+    //     playerIA['generateRandomNumber'] = jasmine.createSpy().and.returnValue(22);
     //     playerIA['setStrategy']();
     //     expect(playerIA.strategy).toBeInstanceOf(SkipTurn);
     // });
 
-//     it('should have a scoring range', () => {
-//         spyOn<any>(playerIA, 'generateRandomNumber').and.returnValues(0, 1, 4, 22);
+    it('should have a scoring range', () => {
+        spyOn<any>(playerIA, 'generateRandomNumber').and.returnValues(0, 1, 4, 22);
 
-//         expect(playerIA['pointingRange']()).toEqual({ min: 0, max: 6 });
-//         expect(playerIA['pointingRange']()).toEqual({ min: 7, max: 12 });
-//         expect(playerIA['pointingRange']()).toEqual({ min: 13, max: 18 });
-//         expect(playerIA['pointingRange']()).toEqual({ min: 0, max: 0 });
-//     });
+        expect(playerIA['pointingRange']()).toEqual({ min: 0, max: 6 });
+        expect(playerIA['pointingRange']()).toEqual({ min: 7, max: 12 });
+        expect(playerIA['pointingRange']()).toEqual({ min: 13, max: 18 });
+        expect(playerIA['pointingRange']()).toEqual({ min: 0, max: 0 });
+    });
 
-//     it('should return a random number between 0 and a give number', () => {
-//         let MAX_VALUE = 3;
-//         for (let i = 0; i < 10; i++) {
-//             const result = playerIA['generateRandomNumber'](MAX_VALUE);
-//             expect(result).toBeGreaterThanOrEqual(0);
-//             expect(result).toBeLessThan(MAX_VALUE);
-//         }
+    it('should return a random number between 0 and a give number', () => {
+        let MAX_VALUE = 3;
+        for (let i = 0; i < 10; i++) {
+            const result = playerIA['generateRandomNumber'](MAX_VALUE);
+            expect(result).toBeGreaterThanOrEqual(0);
+            expect(result).toBeLessThan(MAX_VALUE);
+        }
 
-//         MAX_VALUE = 10;
-//         for (let i = 0; i < 10; i++) {
-//             const result = playerIA['generateRandomNumber'](MAX_VALUE);
-//             expect(result).toBeGreaterThanOrEqual(0);
-//             expect(result).toBeLessThan(MAX_VALUE);
-//         }
-//     });
+        MAX_VALUE = 10;
+        for (let i = 0; i < 10; i++) {
+            const result = playerIA['generateRandomNumber'](MAX_VALUE);
+            expect(result).toBeGreaterThanOrEqual(0);
+            expect(result).toBeLessThan(MAX_VALUE);
+        }
+    });
 
-//     it('should call the right functions when calling play()', () => {
-//         spyOn<any>(playerIA.strategy, 'execute');
-//         spyOn<any>(playerIA, 'setStrategy');
+    it('should call the right functions when calling play()', () => {
+        spyOn<any>(playerIA.strategy, 'execute');
+        spyOn<any>(playerIA, 'setStrategy');
 
-//         playerIA.play();
+        playerIA.play();
 
-//         expect(playerIA.strategy.execute).toHaveBeenCalledTimes(1);
-//         expect(playerIA['setStrategy']).toHaveBeenCalledTimes(1);
-//     });
+        expect(playerIA.strategy.execute).toHaveBeenCalledTimes(1);
+        expect(playerIA['setStrategy']).toHaveBeenCalledTimes(1);
+    });
 
-//     it('should set the right context when setContext() is called', () => {
-//         const context: PlayerIAComponent = TestBed.createComponent(PlayerIAComponent).componentInstance;
-//         playerIA.setContext(context);
-//         expect(playerIA.context).toEqual(context);
-//     });
-// });
+    it('should set the right context when setContext() is called', () => {
+        const context: PlayerIAComponent = TestBed.createComponent(PlayerIAComponent).componentInstance;
+        playerIA.setContext(context);
+        expect(playerIA.context).toEqual(context);
+    });
+});
--- client/src/app/modules/game-view/components/scrabble-board/scrabble-board.component.spec.ts
+++ client/src/app/modules/game-view/components/scrabble-board/scrabble-board.component.spec.ts
@@ -1,11 +1,10 @@
 /* eslint-disable dot-notation */
-/* eslint-disable prettier/prettier */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MouseHandlerService } from '@app/services/mouse-handler.service';
 import { ScrabbleBoardComponent } from './scrabble-board.component';
 
 describe('ScrabbleBoardComponent', () => {
-    //let component: ScrabbleBoardComponent;
+    let component: ScrabbleBoardComponent;
     let fixture: ComponentFixture<ScrabbleBoardComponent>;
     let mouseServiceSpy: jasmine.SpyObj<MouseHandlerService>;
 
@@ -22,7 +21,7 @@ describe('ScrabbleBoardComponent', () => {
 
     beforeEach(() => {
         fixture = TestBed.createComponent(ScrabbleBoardComponent);
-        //component = fixture.componentInstance;
+        component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
@@ -31,7 +30,7 @@ describe('ScrabbleBoardComponent', () => {
     });
 
     it('buttonDetect should modify the buttonPressed variable', () => {
-        spyOn(component['gridService'],'setGridContext');
+        spyOn(component['gridService'], 'setGridContext');
         const expectedKey = 'a';
         const buttonEvent = {
             key: expectedKey,
@@ -41,7 +40,7 @@ describe('ScrabbleBoardComponent', () => {
     });
 
     it('mouse hit detect should call mouse hit detect from gridService', () => {
-        spyOn(component['gridService'],'setGridContext');
+        spyOn(component['gridService'], 'setGridContext');
         const mouseEvent = {
             offsetX: 10,
             offsetY: 1,
@@ -50,27 +49,4 @@ describe('ScrabbleBoardComponent', () => {
         component.mouseHitDetect(mouseEvent);
         expect(mouseServiceSpy.mouseHitDetect).toHaveBeenCalledWith(mouseEvent);
     });
-
-    // it('should create', () => {
-    //     expect(component).toBeTruthy();
-    // });
-
-    // it('buttonDetect should modify the buttonPressed variable', () => {
-    //     const expectedKey = 'a';
-    //     const buttonEvent = {
-    //         key: expectedKey,
-    //     } as KeyboardEvent;
-    //     component.buttonDetect(buttonEvent);
-    //     expect(component.buttonPressed).toEqual(expectedKey);
-    // });
-
-    // it('mouse hit detect should call mouse hit detect from gridService', () => {
-    //     const mouseEvent = {
-    //         offsetX: 10,
-    //         offsetY: 1,
-    //         button: 0,
-    //     } as MouseEvent;
-    //     component.mouseHitDetect(mouseEvent);
-    //     expect(mouseServiceSpy.mouseHitDetect).toHaveBeenCalledWith(mouseEvent);
-    // });
 });
