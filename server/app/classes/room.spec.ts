b3e7d9fbcca90ba4a87580d697aa976b6a9b13e0
paako781 <paul-marie.akoffodji@polymtl.ca>
2021-10-28 13:56:16 -0400

    Ajout test  ckient et server

--- server/app/classes/room.spec.ts
+++ server/app/classes/room.spec.ts
@@ -6,26 +6,26 @@ import { Room, State } from './room';
 
 describe('Room', () => {
     const id = 'LOG2990';
-    const ownerName = 'Paul';
+    // const ownerName = 'Paul';
     // const customerName = 'Mike';
     const settings: GameSettings = new GameSettings(['Paul', 'Mike'], StartingPlayer.Owner, '00', '30', 'facile', 'non', 'français');
     const state = State.Waiting;
     let room: Room;
 
     beforeEach(async () => {
-        room = new Room(id, ownerName, settings, state.valueOf());
+        room = new Room(id, settings, state.valueOf());
     });
 
     it('should create an instance', () => {
-        room = new Room(id, ownerName, settings, state.valueOf());
+        room = new Room(id, settings, state.valueOf());
         expect(room.id).to.equal('LOG2990');
-        expect(room.ownerName).to.equal('Paul');
         expect(room.gameSettings).to.equals(settings);
         expect(room.state).to.equal(1);
     });
 
     it('should add customer at the Room', () => {
+        room.gameSettings.playersName[0] = 'Mike';
         room.addCustomer('Paul');
-        expect(room.customerName).to.equal('Paul');
+        expect(room.gameSettings.playersName[1]).to.equal('Paul');
     });
 });
--- server/app/services/room-manager.service.spec.ts
+++ server/app/services/room-manager.service.spec.ts
@@ -2,7 +2,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 // import { State } from '@app/classes/room';
-import { State } from '@app/classes/room';
+// import { State } from '@app/classes/room';
+// import { State } from '@app/classes/room';
 import { RoomManager } from '@app/services/room-manager.service';
 import { expect } from 'chai';
 import { describe } from 'mocha';
@@ -11,7 +12,6 @@ import { GameSettings, StartingPlayer } from '../../../server/app/classes/multip
 describe('RoomManagerService', () => {
     let roomManagerService: RoomManager;
     const id = 'LOG2990';
-    const ownerName = 'Paul';
     const settings: GameSettings = new GameSettings(['Paul', 'Mike'], StartingPlayer.Owner, '00', '30', 'facile', 'non', 'français');
 
     beforeEach(() => {
@@ -20,69 +20,89 @@ describe('RoomManagerService', () => {
     });
 
     it('should create a Room', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
+        roomManagerService.createRoom(id, settings);
         expect(roomManagerService.rooms.length).to.equal(1);
     });
 
     it('should not add customer at the Room if ht name are same', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
+        roomManagerService.createRoom(id, settings);
         const isSame: boolean = roomManagerService.addCustomer('Paul', id);
         expect(isSame).to.equal(false);
     });
-    it('should add customer at the Room if the names are differents', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
-        const isSame: boolean = roomManagerService.addCustomer('Mike', id);
-        expect(isSame).to.equal(true);
-    });
 
     it('should set the state of Room', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
-        roomManagerService.addCustomer('Mike', id);
-        roomManagerService.setState(id, State.Playing);
-        expect(roomManagerService.rooms[0].state).to.equal(State.Playing);
+        roomManagerService.createRoom(id, settings);
+        roomManagerService.rooms[0].gameSettings.playersName[1] = '';
+        const testID = roomManagerService.createRoomId('Paul');
+        roomManagerService.setState(testID, 1);
+        expect(roomManagerService.rooms[0].state).to.equal(1);
     });
 
     it('should return the gamesettings  of room', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
-        roomManagerService.addCustomer('Mike', id);
-        const testSettings = roomManagerService.getGameSettings(id);
+        roomManagerService.createRoom(id, settings);
+        const testID = roomManagerService.createRoomId('Paul');
+        const testSettings = roomManagerService.getGameSettings(testID);
         expect(roomManagerService.rooms[0].gameSettings).to.equal(testSettings);
     });
 
     it('should delete the room with the right id', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
-        roomManagerService.addCustomer('Mike', id);
-        roomManagerService.deleteRoom(id);
+        roomManagerService.createRoom(id, settings);
+        const testID = roomManagerService.createRoomId('Paul');
+        roomManagerService.deleteRoom(testID);
         expect(roomManagerService.rooms.length).to.equal(0);
     });
 
     it('should not delete the room if his ID is not in the table', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
+        roomManagerService.createRoom(id, settings);
         roomManagerService.addCustomer('Mike', id);
         roomManagerService.deleteRoom('LOG2991');
         expect(roomManagerService.rooms.length).to.equal(1);
     });
 
     it('should find the room by the ID and return it', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
-        roomManagerService.addCustomer('Mike', id);
-        expect(roomManagerService.rooms[0]).to.equal(roomManagerService.find(id));
+        roomManagerService.createRoom(id, settings);
+        const testID = roomManagerService.createRoomId('Paul');
+        expect(roomManagerService.rooms[0]).to.equal(roomManagerService.find(testID));
     });
     it('should not find the room by the ID and return undefined', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
+        roomManagerService.createRoom(id, settings);
         roomManagerService.addCustomer('Mike', id);
         expect(roomManagerService.find('LOG2991')).to.equal(undefined);
     });
 
-    it('should true if the ownername and custorname are same', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
-        roomManagerService.addCustomer('Paul', id);
-        expect(roomManagerService.isSameName(roomManagerService.rooms[0], 'Paul')).to.equal(true);
+    it('should  return true if the room is not undefined', () => {
+        roomManagerService.rooms = [];
+        expect(roomManagerService.addCustomer('jojo', id)).to.equal(false);
     });
 
-    it('should false if the ownername and custorname are differents', () => {
-        roomManagerService.createRoom(id, ownerName, settings);
-        roomManagerService.addCustomer('Mike', id);
-        expect(roomManagerService.isSameName(roomManagerService.rooms[0], 'Mike')).to.equal(false);
+    it('should false if the room undefined ', () => {
+        roomManagerService.createRoom(id, settings);
+        roomManagerService.rooms[0].gameSettings.playersName[1] = '';
+        const testID = roomManagerService.createRoomId('Paul');
+        expect(roomManagerService.addCustomer('Mike', testID)).to.equal(true);
+    });
+
+    it('should return the formatGameSetttings', () => {
+        roomManagerService.createRoom(id, settings);
+        const testID = roomManagerService.createRoomId('Paul');
+        expect(roomManagerService.formatGameSettingsForCustomerIn(testID)).not.to.equal(undefined);
+    });
+
+    it('should return the formatGameSetttings truth', () => {
+        roomManagerService.createRoom(id, settings);
+        const testID = roomManagerService.createRoomId('Paul');
+        // const testTable = ['Mike', 'Paul'];
+        // const testFormatSettings = new GameSettings(testTable, 1, '00', '30', 'non', 'non', 'français');
+        expect(roomManagerService.formatGameSettingsForCustomerIn(testID));
+    });
+
+    it('should return  false if the room does not exist', () => {
+        roomManagerService.rooms = [];
+        expect(roomManagerService.isNotAvailable(id)).to.equal(false);
+    });
+    it('should return  true if the room  exist', () => {
+        roomManagerService.createRoom(id, settings);
+        const testID = roomManagerService.createRoomId('Paul');
+        expect(roomManagerService.isNotAvailable(testID)).to.equal(false);
     });
 });
