a0f4c6cc505f6706a71949e8830f36e6051cd8c8
fienfiej <71525018+fienfiej@users.noreply.github.com>
2021-11-05 19:56:42 -0400

    Tests update

--- client/src/app/modules/game-view/game-view/game-view.component.spec.ts
+++ client/src/app/modules/game-view/game-view/game-view.component.spec.ts
@@ -1,7 +1,10 @@
+/* eslint-disable dot-notation */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameViewComponent } from './game-view.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Player } from '@app/models/player.model';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GameViewComponent', () => {
     let component: GameViewComponent;
@@ -10,7 +13,7 @@ describe('GameViewComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [GameViewComponent],
-            imports: [RouterTestingModule],
+            imports: [RouterTestingModule, HttpClientTestingModule],
             schemas: [NO_ERRORS_SCHEMA],
         }).compileComponents();
     });
@@ -18,10 +21,20 @@ describe('GameViewComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(GameViewComponent);
         component = fixture.componentInstance;
+        spyOn(component['playerService'], 'bindUpdateEasel');
         fixture.detectChanges();
+        component['playerService'].addPlayer(new Player(1, 'Player 1', []));
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should update component fontSize and playerService fontSize with new fontSize', () => {
+        spyOn(component['playerService'], 'updateFontSize');
+        const fontSize = 10;
+        component.handleFontSizeEvent(fontSize);
+        expect(component.fontSize).toEqual(fontSize);
+        expect(component['playerService'].updateFontSize).toHaveBeenCalled();
     });
 });
