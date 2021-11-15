c8a46d76b90487180e7f4bdfb3f7805e32b10957
UrsuleJoelle <ursule-joelle.kanmegne-djoum@polymtl.ca>
2021-11-15 12:09:24 -0500

    setup de base connexion BD

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,17 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Db } from 'mongodb';
 import * as mongoose from 'mongoose';
-// import 'reflect-metadata';
 import { Service } from 'typedi';
 
 @Service()
 export class DatabaseService {
     private db: Db;
-    // private client: MongoClient;
-    // CHANGE the URL for your database information
     private readonly databaseUrl = 'mongodb+srv://Team107:xxtyU48kHQ4ZqDW@cluster0.i7hu4.mongodb.net/database1?retryWrites=true&w=majority';
-    /// private readonly databaseName = 'database1';
-    // private readonly databaseCollection = 'collection1';
     private options = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -25,7 +20,6 @@ export class DatabaseService {
             .connect(url, this.options)
             .then(() => {
                 console.log('Connected successfully to Mongodb Atlas');
-                // this.db = this.databaseClient;
             })
             .catch(() => {
                 throw new Error('Distant database connection error');
