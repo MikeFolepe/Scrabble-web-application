4bb6725648ed0773847bd6bcd1e38a6791e62832
fienfiej <71525018+fienfiej@users.noreply.github.com>
2021-11-14 20:14:55 -0500

    Vue 1.0 terminée

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
+
+interface Dictionnary {
+    name: string;
+    description: string;
+    isDefault: boolean;
+}
 
 @Component({
-  selector: 'app-admin-page',
-  templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.scss']
+    selector: 'app-admin-page',
+    templateUrl: './admin-page.component.html',
+    styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent implements OnInit {
+export class AdminPageComponent {
+    @ViewChild(MatAccordion) accordion: MatAccordion;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    dictionaries: Dictionnary[] = [
+        { name: 'Dico #1', description: 'Dictionnaire par défaut', isDefault: true },
+        { name: 'Dico #2', description: 'blabla blablabla #2', isDefault: false },
+        { name: 'Dico #3', description: 'blabla blablabla #3', isDefault: false },
+        { name: 'Dico #4', description: 'blabla blablabla #4', isDefault: false },
+    ];
+    beginnerNames = ['Name #1', 'Name #2', 'Name #3', 'Name #4'];
+    expertNames = ['Name #1', 'Name #2', 'Name #3'];
 }
