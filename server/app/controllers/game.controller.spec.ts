d04a9eba4c86dca09b381d37c9f481d051dd9822
MikeFolepe <78043104+MikeFolepe@users.noreply.github.com>
2021-12-03 02:17:54 -0500

    Ajout des tests de bestscore service

--- server/app/controllers/game.controller.spec.ts
+++ server/app/controllers/game.controller.spec.ts
@@ -8,15 +8,32 @@ import { Container } from 'typedi';
 import * as fileSystem from 'fs';
 import chaiHttp = require('chai-http');
 import Sinon = require('sinon');
+import { BestScoresService } from '@app/services/best-scores.service';
+import { PlayerScore } from '@common/player';
 
 describe('GameController', () => {
     let expressApp: Express.Application;
     let wordValidationService: WordValidationService;
+    let bestScoresService: BestScoresService;
     chai.use(chaiHttp);
+
+    const playersScores: PlayerScore[] = [
+        {
+            score: 15,
+            playerName: 'JoelleTest',
+            isDefault: false,
+        },
+        {
+            score: 20,
+            playerName: 'JojoTest',
+            isDefault: false,
+        },
+    ];
 
     beforeEach(() => {
         const app = Container.get(Application);
         wordValidationService = Container.get<WordValidationService>(WordValidationService);
+        bestScoresService = Container.get<BestScoresService>(BestScoresService);
         expressApp = app.app;
     });
 
@@ -49,8 +66,6 @@ describe('GameController', () => {
     });
 
     it('should return the dictionary asked by the client', (done) => {
-        // fileSystem.readFileSync('./dictionaries/dictionary.json', 'utf8')).words;
-
         const jsonDictionary = `{
             "title": "Mon dictionnaire",
             "description": "Description de base",
@@ -72,6 +87,82 @@ describe('GameController', () => {
                 expect(response.body).to.deep.equal(dictionary.words);
                 expect(response.status).to.equal(StatusCodes.OK);
                 stubOnParse.restore();
+                done();
+            });
+    });
+
+    it('should return the bests players of classic mode', (done) => {
+        const getBestPlayersStub = Sinon.stub(bestScoresService, 'getBestPlayers').returns(Promise.resolve(playersScores));
+        chai.request(expressApp)
+            .get('/api/game/best-scores-classic')
+            .end((err, response) => {
+                expect(getBestPlayersStub.called).to.equal(true);
+                expect(response.status).to.equal(StatusCodes.OK);
+                expect(response.body).to.deep.equal(playersScores);
+                getBestPlayersStub.restore();
+                done();
+            });
+    });
+
+    it('should handle the error while getting bests players of classic mode', (done) => {
+        const getBestPlayersStub = Sinon.stub(bestScoresService, 'getBestPlayers').returns(Promise.reject(new Error('fail')));
+        chai.request(expressApp)
+            .get('/api/game/best-scores-classic')
+            .end((err, response) => {
+                expect(getBestPlayersStub.called).to.equal(true);
+                expect(response.status).to.equal(StatusCodes.NOT_FOUND);
+                getBestPlayersStub.restore();
+                done();
+            });
+    });
+
+    it('should return the bests scores of log2990 mode', (done) => {
+        const getBestPlayersStub = Sinon.stub(bestScoresService, 'getBestPlayers').returns(Promise.resolve(playersScores));
+        chai.request(expressApp)
+            .get('/api/game/best-scores-log2990')
+            .end((err, response) => {
+                expect(getBestPlayersStub.called).to.equal(true);
+                expect(response.status).to.equal(StatusCodes.OK);
+                expect(response.body).to.deep.equal(playersScores);
+                getBestPlayersStub.restore();
+                done();
+            });
+    });
+
+    it('should handle the error while getting bests players of log2990 mode ', (done) => {
+        const getBestPlayersStub = Sinon.stub(bestScoresService, 'getBestPlayers').returns(Promise.reject(new Error('fail')));
+        chai.request(expressApp)
+            .get('/api/game/best-scores-log2990')
+            .end((err, response) => {
+                expect(getBestPlayersStub.called).to.equal(true);
+                expect(response.status).to.equal(StatusCodes.NOT_FOUND);
+                getBestPlayersStub.restore();
+                done();
+            });
+    });
+
+    it('should add players scores of classic mode by calling add addPlayers of bestScoresService', (done) => {
+        const addPlayersStub = Sinon.stub(bestScoresService, 'addPlayers').returns(Promise.resolve());
+        chai.request(expressApp)
+            .post('/api/game/best-scores-classic')
+            .send(playersScores)
+            .end((err, response) => {
+                expect(addPlayersStub.called).to.equal(true);
+                expect(response.status).to.equal(StatusCodes.OK);
+                addPlayersStub.restore();
+                done();
+            });
+    });
+
+    it('should add players scores of log2990 mode by calling add addPlayers of bestScoresService', (done) => {
+        const addPlayersStub = Sinon.stub(bestScoresService, 'addPlayers').returns(Promise.resolve());
+        chai.request(expressApp)
+            .post('/api/game/best-scores-log2990')
+            .send(playersScores)
+            .end((err, response) => {
+                expect(addPlayersStub.called).to.equal(true);
+                expect(response.status).to.equal(StatusCodes.OK);
+                addPlayersStub.restore();
                 done();
             });
     });
